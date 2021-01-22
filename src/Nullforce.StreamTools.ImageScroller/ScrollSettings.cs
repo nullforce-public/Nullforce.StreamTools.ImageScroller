65394fe8fbbccf4b76315fa5620920d268da1a74
nullforce <glenngit@nullforce.com>
2021-01-21 23:37:37 -0600

    Add font color and show filename options

--- src/Nullforce.StreamTools.ImageScroller/ScrollSettings.cs
+++ src/Nullforce.StreamTools.ImageScroller/ScrollSettings.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Nullforce.StreamTools.ImageScroller
+﻿namespace Nullforce.StreamTools.ImageScroller
 {
     public class ScrollSettings
     {
+        public string FontColor { get; set; }
         public int ImageCount { get; set; }
         public string ImageLocation { get; set; }
         public int ImageMaxHeight { get; set; }
         public int ImageMaxWidth { get; set; }
         public int IntervalInSeconds { get; set; }
+        public bool ShowFilename { get; set; }
         public bool ShuffleImages { get; set; }
     }
 }
